c04b85965a9cfd3ba1c3d598749372b99a0b188c
William <billy.ryan.rom@gmail.com>
2020-08-21 12:26:27 -0400

    Refactor cpu.py prettify code base * Add class functions Call and Ret

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,26 +1,36 @@
+"""CPU functionality."""
+
 import sys
 
 class CPU:
+    """Main CPU class."""
+
     def __init__(self):
-        # Memory and general purpose registers
-        self.ram = [None] * 256
-        self.register = [0] * 8
-
-        # Program Counter
-        self.pc = 0 
-
-    def ram_read(self, MAR):
-        return self.ram[MAR]
-
-    def ram_write(self, MAR, MDR):
-        self.ram[MAR] = MDR
-        return None
+        """Construct a new CPU."""
+        self.ram = [0] * 256
+        self.reg = [0] * 8 
+        self.sp = 7
+        self.reg[self.sp] = 0xF4
+        self.pc = 0
+        self.ir = 0
+        self.fl = 6
+        self.op_table = {
+                        0b10000010 : self.ldi, 
+                        0b01000111 : self.prn, 
+                        0b10100010 : self.mult,
+                        0b00000001 : self.hlt,
+                        0b01000101 : self.push,
+                        0b01000110 : self.pop,
+                        0b01010000 : self.call, 
+                        0b00010001 : self.ret,
+                        0b10100000 : self.add,
+                    }
 
     def load(self):
         """Load a program into memory."""
         try:
             address = 0
-            with open("c:/Users/billy/web27/Computer-Architecture/ls8/examples/stack.ls8") as f:
+            with open("c:/Users/billy/web27/Computer-Architecture/ls8/examples/call.ls8") as f:
                 for line in f:
                     comment_split = line.split("#")
                     n = comment_split[0].strip()
@@ -32,19 +42,58 @@ class CPU:
                     self.ram[address] = val
 
                     address += 1
-
+        
         except FileNotFoundError:
             print(f"{sys.argv[0]}: {filename} not found")
             sys.exit(2)
 
+    def ldi(self, op_a, op_b):
+        self.reg[op_a] = op_b
+
+    def mult(self, op_a, op_b):
+        self.reg[op_a] = self.reg[op_a] * self.reg[op_b]
+
+    # Push the value in the given register on the stack.
+    def push(self, op_a, op_b):        
+        self.reg[self.sp] -= 1
+        self.ram[self.reg[self.sp]] = self.reg[op_a]
+
+    # Pop the value at the top of the stack into the given register.
+    def pop(self, op_a, op_b):
+        self.reg[op_a] = self.ram[self.reg[self.sp]]
+        self.reg[self.sp] += 1
+
+    # Sets the PC to the register value
+    def call(self, op_a, op_b):
+        self.reg[self.sp] -= 1
+        self.ram[self.reg[self.sp]] = self.pc + 2
+        self.pc = self.reg[op_a]
+        return True
+
+    def ret(self, op_a, op_b):
+        self.pop(op_a, 0)
+        self.pc = self.reg[op_a]
+        return True
+
+    def add(self, op_a, op_b):
+        self.reg[op_a] = self.reg[op_a] + self.reg[op_b]
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
+    def prn(self, op_a, op_b):
+        print(self.reg[op_a])
+
+    def hlt(self, op_a, op_b):
+        sys.exit()
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
-            self.register[reg_a] += self.register[reg_b]
-        elif op == "MUL":
-            self.register[reg_a] *= self.register[reg_b]
+            self.reg[reg_a] += self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -53,74 +102,27 @@ class CPU:
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
             #self.fl,
             #self.ie,
-            self.ram_read(self.pp),
+            self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
         ), end='')
 
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
-        print()
 
+        print()
 
     def run(self):
         """Run the CPU."""
-        program_counter = self.pc
-        instruction = self.ram[program_counter] # Grabbing instruction from memory based on program counter
-        run = True
-        stack_pointer = len(self.register) - 1
+        while True:
+            self.ir = self.ram[self.pc]
+            op_a = self.ram[self.pc + 1]
+            op_b = self.ram[self.pc + 2]
 
-        while run:
-            # Grabbing next two instructions in case they're needed using ram_read
-            operand_a = self.ram_read(program_counter + 1)
-            operand_b = self.ram_read(program_counter + 2)
-
-            ''' IF/ELSE CLAUSES '''
-            # HLT - Halt command
-            if instruction == 0b00000001:
-                run = False
- 
-            # LDI - Set the value of a register to an integer.
-            elif instruction == 0b10000010:
-                self.register[operand_a] = operand_b # This sets register a with the value b (0,8)
-                self.pc += 3
-
-            # PRN - Prints the next opcode    
-            elif instruction == 0b01000111:
-                self.pc += 2
-                print(self.register[operand_a])
-
-            # MUL - Multiply 2 registers together and save result to the first register (SHOULD USE ALU)
-            elif instruction == 0b10100010:
-                self.pc += 3
-                self.alu('MUL', operand_a, operand_b)
-            
-            # PUSH
-            elif instruction == 0b01000101:
-                stack_pointer -= 1 # Decrement the index for our stack in ram
-                self.ram[stack_pointer] = self.register[operand_a]
-                self.pc += 2
-
-            # POP
-            elif instruction == 0b01000110:
-                '''
-                1. Copy the value from the address pointed to by `SP` to the given register.
-                2. Increment `SP`.
-                '''
-                if stack_pointer < 244:
-                    self.register[operand_a] = self.ram[stack_pointer]
-                    stack_pointer += 1
-                else:
-                    print('Cant push onto an empty stack!')
-
-                self.pc += 2
-
-
-            # Point Counter Update/Run update
-            program_counter = self.pc # Get new Program Counter
-            instruction = self.ram[program_counter] # Grab Instruction
+            willJump = self.op_table[self.ir](op_a, op_b)
+            if not willJump:
+                self.pc += (self.ir >> 6) + 1
---
+++ ls8/notes.md
@@ -0,0 +1,5 @@
+1. Ins and outs of subroutines
+
+2. Whats the result of bitwise or and and between
+
+3. Convert binary into hex
